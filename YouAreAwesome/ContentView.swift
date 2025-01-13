8e11f6e0e8e78bc915c8e94fa5649d2e50db5bb2
oleh <phd.chornyi@gmail.com>
2025-01-13 11:44:53 +0200

    4th: Layauting

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,19 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "When the Genious Bar needs help, they call you!"
+    
     var body: some View {
         
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            Text("You Are Awesome!")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+        ZStack {
+//            Color.yellow.opacity(0.5)
+            Rectangle()
+//                .fill(.yellow.gradient)
+                .fill(Gradient(colors: [.orange, .yellow]))
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("You have skills")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(Color("SkyBlue"))
+                    .cornerRadius(15)
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .padding()
+    //                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                
+                Spacer()
+                HStack(content: {
+                    Button("Press Me") {
+                        messageString = "You Are Awesome"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great"
+                    }
+                    .buttonStyle(.borderedProminent)
+                })
+    //            .border(.purple, width: 5)
+                .padding()
+                
+            }
+            .padding()
         }
-        .padding()
         
     }
 }
