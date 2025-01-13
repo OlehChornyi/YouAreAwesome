5413c777228305cc4a3c9360fcd04538f402002d
oleh <phd.chornyi@gmail.com>
2025-01-13 12:05:07 +0200

    5th: Divider

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,57 +12,69 @@ struct ContentView: View {
     
     var body: some View {
         
-        ZStack {
-//            Color.yellow.opacity(0.5)
-            Rectangle()
-//                .fill(.yellow.gradient)
-                .fill(Gradient(colors: [.orange, .yellow]))
-                .ignoresSafeArea()
-            
-            VStack {
-                Text("You have skills")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundStyle(.white)
-                    .padding()
-                    .background(Color("SkyBlue"))
-                    .cornerRadius(15)
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .padding()
-    //                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                
-                Spacer()
-                HStack(content: {
-                    Button("Press Me") {
-                        messageString = "You Are Awesome"
-                    }
-                    .buttonStyle(.borderedProminent)
+//        ZStack {
+////            Color.yellow.opacity(0.5)
+//            Rectangle()
+////                .fill(.yellow.gradient)
+//                .fill(Gradient(colors: [.orange, .yellow]))
+//                .ignoresSafeArea()
+//            
+//            GeometryReader {geometry in
+                VStack {
+//                    Text("You have skills")
+//                        .font(.largeTitle)
+//                        .fontWeight(.black)
+//                        .foregroundStyle(.white)
+//                        .padding()
+//                        .background(Color("SkyBlue"))
+//                        .cornerRadius(15)
+                    Spacer()
                     
-                Spacer()
+                    Text(messageString)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.red)
+                        .padding()
+        //                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                        .frame(height: 150)
+                        .frame(maxWidth: .infinity)
                     
-                    Button("Great") {
-                        messageString = "You Are Great"
-                    }
-                    .buttonStyle(.borderedProminent)
-                })
-    //            .border(.purple, width: 5)
+                    Spacer()
+                    
+//                    Divider()
+//                        .background(.black)
+//                        .padding()
+//                        .frame(width: 150.0)
+//                    
+//                    Rectangle()
+//                        .fill(.indigo)
+//                        .frame(width: geometry.size.width * (2/3), height: 4)
+//
+                    HStack(content: {
+                        Button("Press Me") {
+                            messageString = "You Are Awesome"
+                        }
+                        .buttonStyle(.borderedProminent)
+                        
+                    Spacer()
+                        
+                        Button("Great") {
+                            messageString = "You Are Great"
+                        }
+                        .buttonStyle(.borderedProminent)
+                    })
+        //            .border(.purple, width: 5)
+                    .padding()
+                    
+                }
                 .padding()
-                
             }
-            .padding()
         }
         
-    }
-}
+//    }
+//}
 
 #Preview {
     ContentView()
